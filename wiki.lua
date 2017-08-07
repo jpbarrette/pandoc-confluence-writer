c08d3cd20122f0959e71e166bfc823bb737a288e
Jean-Philippe Barrette-LaPierre <jpb@rrette.com>
2017-08-07 12:47:40 +0200

    First step of using confluence storage format

--- wiki.lua
+++ wiki.lua
@@ -1,3 +1,4 @@
+-- This is a sample custom writer for pandoc.  It produces output
 -- that is very similar to that of pandoc's HTML writer.
 -- There is one new feature: code blocks marked with class 'dot'
 -- are piped through graphviz and images are included in the HTML
@@ -11,65 +12,79 @@
 -- produce informative error messages if your code contains
 -- syntax errors.
 
-local image_index = 0
-
--- Table to store footnotes, so they can be included at the end.
-local notes = {}
-
 -- Character escaping
 local function escape(s, in_attribute)
-   return s:gsub("[<>&\"']",
-                 function(x)
-                    if x == '<' then
-                       return '&lt;'
-                    elseif x == '>' then
-                       return '&gt;'
-                    elseif x == '&' then
-                       return '&amp;'
-                    elseif x == '"' then
-                       return '&quot;'
-                    elseif x == "'" then
-                       return '&#39;'
-                    else
-                       return x
-                    end
-                 end)
+  return s:gsub("[<>&\"']",
+    function(x)
+      if x == '<' then
+        return '&lt;'
+      elseif x == '>' then
+        return '&gt;'
+      elseif x == '&' then
+        return '&amp;'
+      elseif x == '"' then
+        return '&quot;'
+      elseif x == "'" then
+        return '&#39;'
+      else
+        return x
+      end
+    end)
+end
+
+-- Helper function to convert an attributes table into
+-- a string that can be put into HTML tags.
+local function attributes(attr)
+  local attr_table = {}
+  for x,y in pairs(attr) do
+    if y and y ~= "" then
+      table.insert(attr_table, ' ' .. x .. '="' .. escape(y,true) .. '"')
+    end
+  end
+  return table.concat(attr_table)
 end
 
 -- Run cmd on a temporary file containing inp and return result.
 local function pipe(cmd, inp)
-   local tmp = os.tmpname()
-   local tmph = io.open(tmp, "w")
-   tmph:write(inp)
-   tmph:close()
-   local outh = io.popen(cmd .. " " .. tmp,"r")
-   local result = outh:read("*all")
-   outh:close()
-   os.remove(tmp)
-   return result
+  local tmp = os.tmpname()
+  local tmph = io.open(tmp, "w")
+  tmph:write(inp)
+  tmph:close()
+  local outh = io.popen(cmd .. " " .. tmp,"r")
+  local result = outh:read("*all")
+  outh:close()
+  os.remove(tmp)
+  return result
 end
+
+-- Table to store footnotes, so they can be included at the end.
+local notes = {}
 
 -- Blocksep is used to separate block elements.
 function Blocksep()
-   return "\n"
+  return "\n\n"
 end
 
 -- This function is called once for the whole document. Parameters:
 -- body is a string, metadata is a table, variables is a table.
--- One could use some kind of templating
--- system here; this just gives you a simple standalone HTML file.
+-- This gives you a fragment.  You could use the metadata table to
+-- fill variables in a custom lua template.  Or, pass `--template=...`
+-- to pandoc, and pandoc will add do the template processing as
+-- usual.
 function Doc(body, metadata, variables)
-   local buffer = {}
-   local function add(s)
-      table.insert(buffer, s)
-   end
-   add(body)
-   if #notes > 0 then
-      for _,note in pairs(notes) do
-          add("*" .. note)
-      end
+  local buffer = {}
+  local function add(s)
+    table.insert(buffer, s)
   end
-   return table.concat(buffer,'\n')
+  add(body)
+  if #notes > 0 then
+    add('<ol class="footnotes">')
+    for _,note in pairs(notes) do
+      add(note)
+    end
+    add('</ol>')
+  end
+  return table.concat(buffer,'\n') .. '\n'
 end
 
 -- The functions that follow render corresponding pandoc elements.
@@ -78,124 +93,66 @@ end
 -- Comments indicate the types of other variables.
 
 function Str(s)
-   return escape(s)
+  return escape(s)
 end
 
 function Space()
-   return " "
+  return " "
+end
+
+function SoftBreak()
+  return "\n"
 end
 
 function LineBreak()
-   return "\n"
-end
-
-function Para(s)
-   return s .. "\n"
-end
-
-function Plain(s)
-   return s
+  return "<br/>"
 end
 
 function Emph(s)
-   return "_" .. s .. "_"
+  return "<em>" .. s .. "</em>"
 end
 
 function Strong(s)
-   return "*" .. s .. "*"
+  return "<strong>" .. s .. "</strong>"
 end
 
 function Subscript(s)
-   return "~" .. s .. "~"
+  return "<sub>" .. s .. "</sub>"
 end
 
 function Superscript(s)
-   return "^" .. s .. "^"
+  return "<sup>" .. s .. "</sup>"
+end
+
+function SmallCaps(s)
+  return '<span style="font-variant: small-caps;">' .. s .. '</span>'
 end
 
 function Strikeout(s)
-   return '-' .. s .. '-'
+  return '<del>' .. s .. '</del>'
 end
 
-function Link(s, src, tit)
-   return "[" .. s .. "|" .. escape(src,true) .. " " .. escape(tit,true) .. "]"
+function Link(s, src, tit, attr)
+  return "<a href='" .. escape(src,true) .. "' title='" ..
+         escape(tit,true) .. "'>" .. s .. "</a>"
 end
 
-function CaptionedImage(src, s, tit)
-	image_index = image_index + 1
-	return "!" .. escape(src, true) .. "!\n" .. 'FIGURE ' .. image_index .. ". " .. tit .. "\n"
+function Image(s, src, tit, attr)
+  return "<img src='" .. escape(src,true) .. "' title='" ..
+         escape(tit,true) .. "'/>"
 end
 
-function Image(s, src, tit)
-   return "!" .. escape(src,true) .. "!"
+function Code(s, attr)
+  return "<code" .. attributes(attr) .. ">" .. escape(s) .. "</code>"
 end
 
--- lev is an integer, the header level.
-function Header(lev, s, attr)
-   return "h" .. lev .. ". " .. s .. ""
+function InlineMath(s)
+  return "\\(" .. escape(s) .. "\\)"
 end
 
-function BlockQuote(s)
-   return "{quote}\n" .. s .. "\n{quote}"
+function DisplayMath(s)
+  return "\\[" .. escape(s) .. "\\]"
 end
-
-function HorizontalRule()
-   return "----"
-end
-
-function Code(s)
-    return '{{' .. s .. '}}'
-end
-
-function CodeBlock(s, attr)
-   return "{code:" .. attr["class"] .. "}" .. s .. "{code}"
-end
-
-function BulletList(items)
-   local buffer = {}
-   for _, item in pairs(items) do
-      for line in string.gmatch(item, '([^\n]+)') do
-         table.insert(buffer, "*" .. line .. "\n")
-      end
-   end
-   return "\n" .. table.concat(buffer, "") .. "\n"
-end
-
-function OrderedList(items)
-   local buffer = {}
-   for _, item in pairs(items) do
-      for line in string.gmatch(item, '([^\n]+)') do
-         table.insert(buffer, "#" .. line .. "\n")
-      end
-   end
-   return "\n" .. table.concat(buffer, "\n") .. "\n"
-end
-
--- Caption is a string, aligns is an array of strings,
--- widths is an array of floats, headers is an array of
--- strings, rows is an array of arrays of strings.
-function Table(caption, aligns, widths, headers, rows)
-   local buffer = {}
-   local function add(s)
-      table.insert(buffer, s)
-   end
-   local header_row = {}
-   local empty_header = true
-   for i, h in pairs(headers) do
-      table.insert(header_row, h)
-      empty_header = empty_header and h == ""
-   end
-   if empty_header then
-      head = ""
-   else
-      add('|| ' .. table.concat(header_row, ' || ') .. ' ||')
-   end
-   for _, row in pairs(rows) do
-      add('| ' .. table.concat(row, ' | ') .. ' |')
-   end
-   return "\n" .. table.concat(buffer,'\n') .. "\n"
-end
-
 
 function Note(s)
   local num = #notes + 1
@@ -209,15 +166,180 @@ function Note(s)
             '"><sup>' .. num .. '</sup></a>'
 end
 
+function Span(s, attr)
+  return "<span" .. attributes(attr) .. ">" .. s .. "</span>"
+end
 
+function RawInline(format, str)
+  if format == "html" then
+    return str
+  else
+    return ''
+  end
+end
+
+function Cite(s, cs)
+  local ids = {}
+  for _,cit in ipairs(cs) do
+    table.insert(ids, cit.citationId)
+  end
+  return "<span class=\"cite\" data-citation-ids=\"" .. table.concat(ids, ",") ..
+    "\">" .. s .. "</span>"
+end
+
+function Plain(s)
+  return s
+end
+
+function Para(s)
+  return "<p>" .. s .. "</p>"
+end
+
+-- lev is an integer, the header level.
+function Header(lev, s, attr)
+  return "<h" .. lev .. attributes(attr) ..  ">" .. s .. "</h" .. lev .. ">"
+end
+
+function BlockQuote(s)
+  return "<blockquote>\n" .. s .. "\n</blockquote>"
+end
+
+function HorizontalRule()
+  return "<hr/>"
+end
+
+function LineBlock(ls)
+  return '<div style="white-space: pre-line;">' .. table.concat(ls, '\n') ..
+         '</div>'
+end
+
+function CodeBlock(s, attr)
+  -- If code block has class 'dot', pipe the contents through dot
+  -- and base64, and include the base64-encoded png as a data: URL.
+  if attr.class and string.match(' ' .. attr.class .. ' ',' dot ') then
+    local png = pipe("base64", pipe("dot -Tpng", s))
+    return '<img src="data:image/png;base64,' .. png .. '"/>'
+  -- otherwise treat as code (one could pipe through a highlighter)
+  else
+    return "<pre><code" .. attributes(attr) .. ">" .. escape(s) ..
+           "</code></pre>"
+  end
+end
+
+function BulletList(items)
+  local buffer = {}
+  for _, item in pairs(items) do
+    table.insert(buffer, "<li>" .. item .. "</li>")
+  end
+  return "<ul>\n" .. table.concat(buffer, "\n") .. "\n</ul>"
+end
+
+function OrderedList(items)
+  local buffer = {}
+  for _, item in pairs(items) do
+    table.insert(buffer, "<li>" .. item .. "</li>")
+  end
+  return "<ol>\n" .. table.concat(buffer, "\n") .. "\n</ol>"
+end
+
+-- Revisit association list STackValue instance.
+function DefinitionList(items)
+  local buffer = {}
+  for _,item in pairs(items) do
+    for k, v in pairs(item) do
+      table.insert(buffer,"<dt>" .. k .. "</dt>\n<dd>" ..
+                        table.concat(v,"</dd>\n<dd>") .. "</dd>")
+    end
+  end
+  return "<dl>\n" .. table.concat(buffer, "\n") .. "\n</dl>"
+end
+
+-- Convert pandoc alignment to something HTML can use.
+-- align is AlignLeft, AlignRight, AlignCenter, or AlignDefault.
+function html_align(align)
+  if align == 'AlignLeft' then
+    return 'left'
+  elseif align == 'AlignRight' then
+    return 'right'
+  elseif align == 'AlignCenter' then
+    return 'center'
+  else
+    return 'left'
+  end
+end
+
+function CaptionedImage(src, tit, caption, attr)
+   return '<div class="figure">\n<img src="' .. escape(src,true) ..
+      '" title="' .. escape(tit,true) .. '"/>\n' ..
+      '<p class="caption">' .. caption .. '</p>\n</div>'
+end
+
+-- Caption is a string, aligns is an array of strings,
+-- widths is an array of floats, headers is an array of
+-- strings, rows is an array of arrays of strings.
+function Table(caption, aligns, widths, headers, rows)
+  local buffer = {}
+  local function add(s)
+    table.insert(buffer, s)
+  end
+  add("<table>")
+  if caption ~= "" then
+    add("<caption>" .. caption .. "</caption>")
+  end
+  if widths and widths[1] ~= 0 then
+    for _, w in pairs(widths) do
+      add('<col width="' .. string.format("%d%%", w * 100) .. '" />')
+    end
+  end
+  local header_row = {}
+  local empty_header = true
+  for i, h in pairs(headers) do
+    local align = html_align(aligns[i])
+    table.insert(header_row,'<th align="' .. align .. '">' .. h .. '</th>')
+    empty_header = empty_header and h == ""
+  end
+  if empty_header then
+    head = ""
+  else
+    add('<tr class="header">')
+    for _,h in pairs(header_row) do
+      add(h)
+    end
+    add('</tr>')
+  end
+  local class = "even"
+  for _, row in pairs(rows) do
+    class = (class == "even" and "odd") or "even"
+    add('<tr class="' .. class .. '">')
+    for i,c in pairs(row) do
+      add('<td align="' .. html_align(aligns[i]) .. '">' .. c .. '</td>')
+    end
+    add('</tr>')
+  end
+  add('</table')
+  return table.concat(buffer,'\n')
+end
+
+function RawBlock(format, str)
+  if format == "html" then
+    return str
+  else
+    return ''
+  end
+end
+
+function Div(s, attr)
+  return "<div" .. attributes(attr) .. ">\n" .. s .. "</div>"
+end
 
 -- The following code will produce runtime warnings when you haven't defined
 -- all of the functions you need for the custom writer, so it's useful
 -- to include when you're working on a writer.
 local meta = {}
-meta.__index = function(_, key)
-   io.stderr:write(string.format("WARNING: Undefined function '%s'\n",key))
-   return function() return "" end
-end
+meta.__index =
+  function(_, key)
+    io.stderr:write(string.format("WARNING: Undefined function '%s'\n",key))
+    return function() return "" end
+  end
 setmetatable(_G, meta)
 
