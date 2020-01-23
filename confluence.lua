8667a52089608525c63cab7e36d885e720e737ec
Konstantin Vanyushov <KVanyushov@gmail.com>
2020-01-23 15:36:47 +1100

    fixed processing of markdown tables

--- confluence.lua
+++ confluence.lua
@@ -14,31 +14,33 @@
 
 -- Character escaping
 local function escape(s, in_attribute)
-  return s:gsub("[<>&\"']",
+  return s:gsub(
+    '[<>&"\']',
     function(x)
-      if x == '<' then
-        return '&lt;'
-      elseif x == '>' then
-        return '&gt;'
-      elseif x == '&' then
-        return '&amp;'
+      if x == "<" then
+        return "&lt;"
+      elseif x == ">" then
+        return "&gt;"
+      elseif x == "&" then
+        return "&amp;"
       elseif x == '"' then
-        return '&quot;'
+        return "&quot;"
       elseif x == "'" then
-        return '&#39;'
+        return "&#39;"
       else
         return x
       end
-    end)
+    end
+  )
 end
 
 -- Helper function to convert an attributes table into
 -- a string that can be put into HTML tags.
 local function attributes(attr)
   local attr_table = {}
-  for x,y in pairs(attr) do
+  for x, y in pairs(attr) do
     if y and y ~= "" then
-      table.insert(attr_table, ' ' .. x .. '="' .. escape(y,true) .. '"')
+      table.insert(attr_table, " " .. x .. '="' .. escape(y, true) .. '"')
     end
   end
   return table.concat(attr_table)
@@ -50,7 +52,7 @@ local function pipe(cmd, inp)
   local tmph = io.open(tmp, "w")
   tmph:write(inp)
   tmph:close()
-  local outh = io.popen(cmd .. " " .. tmp,"r")
+  local outh = io.popen(cmd .. " " .. tmp, "r")
   local result = outh:read("*all")
   outh:close()
   os.remove(tmp)
@@ -76,7 +78,8 @@ function Doc(body, metadata, variables)
   local function add(s)
     table.insert(buffer, s)
   end
-  local css_style = [[
+  local css_style =
+    [[
 <ac:structured-macro ac:macro-id="b7c0ac99-8feb-49c2-858c-870cd55df16b" ac:name="html" ac:schema-version="1">'
     <ac:parameter ac:name="atlassian-macro-output-type">INLINE</ac:parameter>
     <ac:plain-text-body>
@@ -90,12 +93,12 @@ function Doc(body, metadata, variables)
   add(body)
   if #notes > 0 then
     add('<ol class="footnotes">')
-    for _,note in pairs(notes) do
+    for _, note in pairs(notes) do
       add(note)
     end
-    add('</ol>')
+    add("</ol>")
   end
-  return table.concat(buffer,'\n') .. '\n'
+  return table.concat(buffer, "\n") .. "\n"
 end
 
 -- The functions that follow render corresponding pandoc elements.
@@ -136,33 +139,29 @@ function Superscript(s)
 end
 
 function SmallCaps(s)
-  return '<span style="font-variant: small-caps;">' .. s .. '</span>'
+  return '<span style="font-variant: small-caps;">' .. s .. "</span>"
 end
 
 function Strikeout(s)
-  return '<span style="text-decoration: line-through;">' .. s .. '</span>'
+  return '<span style="text-decoration: line-through;">' .. s .. "</span>"
 end
 
 function Link(s, src, tit, attr)
   if src and string.sub(src, 1, 11) == "confluence:" then
-     -- [Page Link](confluence:SPACE:Content Title)
-     for space, page in src:gsub('%%20',' '):gmatch('confluence:(%w+):([%w%s]+)')
-     do
-        return string.format('<ac:link><ri:page ri:space-key="%s" '..
-                             'ri:content-title="%s" /></ac:link>', space, page)
-     end
+    -- [Page Link](confluence:SPACE:Content Title)
+    for space, page in src:gsub("%%20", " "):gmatch("confluence:(%w+):([%w%s]+)") do
+      return string.format('<ac:link><ri:page ri:space-key="%s" ' .. 'ri:content-title="%s" /></ac:link>', space, page)
+    end
   elseif src and string.sub(src, 1, 1) == "#" then
     -- [Anchor Link](#anchor), taken from https://confluence.atlassian.com/doc/confluence-storage-format-790796544.html#ConfluenceStorageFormat-Links
     return LinkToAnchor(escape(string.sub(src, 2, -1), true), s)
   else
-    return "<a href='" .. escape(src,true) .. "' title='" ..
-           escape(tit,true) .. "'>" .. s .. "</a>"
+    return "<a href='" .. escape(src, true) .. "' title='" .. escape(tit, true) .. "'>" .. s .. "</a>"
   end
 end
 
 function Image(s, src, tit, attr)
-  return "<img src='" .. escape(src,true) .. "' title='" ..
-         escape(tit,true) .. "'/>"
+  return "<img src='" .. escape(src, true) .. "' title='" .. escape(tit, true) .. "'/>"
 end
 
 function Code(s, attr)
@@ -178,19 +177,20 @@ function DisplayMath(s)
 end
 
 function AnchorRef(anchorName)
-  return '<ac:structured-macro ac:name="anchor"><ac:parameter ac:name="">'.. anchorName .. '</ac:parameter></ac:structured-macro>'
+  return '<ac:structured-macro ac:name="anchor"><ac:parameter ac:name="">' ..
+    anchorName .. "</ac:parameter></ac:structured-macro>"
 end
 
 function LinkToAnchor(anchorName, text)
-  return '<ac:link ac:anchor="' .. anchorName .. '"><ac:link-body>' .. text .. '</ac:link-body></ac:link>'
+  return '<ac:link ac:anchor="' .. anchorName .. '"><ac:link-body>' .. text .. "</ac:link-body></ac:link>"
 end
 
 function Note(s)
   local num = #notes + 1
   -- add a list item with the note to t[he note table.
-  table.insert(notes, '<li>' .. AnchorRef("fn" .. num) .. s .. LinkToAnchor('fnref' .. num, '&#8617;') .. '</li>')
+  table.insert(notes, "<li>" .. AnchorRef("fn" .. num) .. s .. LinkToAnchor("fnref" .. num, "&#8617;") .. "</li>")
   -- return the footnote reference, linked to the note.
-  return '<sup>' .. AnchorRef('fnref' .. num) .. LinkToAnchor('fn' .. num, num) .. '</sup>'
+  return "<sup>" .. AnchorRef("fnref" .. num) .. LinkToAnchor("fn" .. num, num) .. "</sup>"
 end
 
 function Span(s, attr)
@@ -201,17 +201,16 @@ function RawInline(format, str)
   if format == "html" then
     return str
   else
-    return ''
+    return ""
   end
 end
 
 function Cite(s, cs)
   local ids = {}
-  for _,cit in ipairs(cs) do
+  for _, cit in ipairs(cs) do
     table.insert(ids, cit.citationId)
   end
-  return "<span class=\"cite\" data-citation-ids=\"" .. table.concat(ids, ",") ..
-    "\">" .. s .. "</span>"
+  return '<span class="cite" data-citation-ids="' .. table.concat(ids, ",") .. '">' .. s .. "</span>"
 end
 
 function Plain(s)
@@ -224,10 +223,9 @@ end
 
 -- lev is an integer, the header level.
 function Header(lev, s, attr)
-
   local attr_table = {}
   local prefix = ""
-  for x,y in pairs(attr) do
+  for x, y in pairs(attr) do
     if y and y ~= "" then
       if x == "id" then
         prefix = prefix .. AnchorRef(y)
@@ -237,7 +235,7 @@ function Header(lev, s, attr)
     end
   end
 
-  return prefix .. "<h" .. lev .. attributes(attr_table) ..  ">" .. s .. "</h" .. lev .. ">"
+  return prefix .. "<h" .. lev .. attributes(attr_table) .. ">" .. s .. "</h" .. lev .. ">"
 end
 
 function BlockQuote(s)
@@ -249,20 +247,18 @@ function HorizontalRule()
 end
 
 function LineBlock(ls)
-  return '<div style="white-space: pre-line;">' .. table.concat(ls, '\n') ..
-         '</div>'
+  return '<div style="white-space: pre-line;">' .. table.concat(ls, "\n") .. "</div>"
 end
 
 function CodeBlock(s, attr)
   -- If code block has class 'dot', pipe the contents through dot
   -- and base64, and include the base64-encoded png as a data: URL.
-  if attr.class and string.match(' ' .. attr.class .. ' ',' dot ') then
+  if attr.class and string.match(" " .. attr.class .. " ", " dot ") then
+    -- otherwise treat as code (one could pipe through a highlighter)
     local png = pipe("base64", pipe("dot -Tpng", s))
     return '<img src="data:image/png;base64,' .. png .. '"/>'
-  -- otherwise treat as code (one could pipe through a highlighter)
   else
-    return "<pre><code" .. attributes(attr) .. ">" .. escape(s) ..
-           "</code></pre>"
+    return "<pre><code" .. attributes(attr) .. ">" .. escape(s) .. "</code></pre>"
   end
 end
 
@@ -285,10 +281,9 @@ end
 -- Revisit association list STackValue instance.
 function DefinitionList(items)
   local buffer = {}
-  for _,item in pairs(items) do
+  for _, item in pairs(items) do
     for k, v in pairs(item) do
-      table.insert(buffer,"<dt>" .. k .. "</dt>\n<dd>" ..
-                        table.concat(v,"</dd>\n<dd>") .. "</dd>")
+      table.insert(buffer, "<dt>" .. k .. "</dt>\n<dd>" .. table.concat(v, "</dd>\n<dd>") .. "</dd>")
     end
   end
   return "<dl>\n" .. table.concat(buffer, "\n") .. "\n</dl>"
@@ -297,37 +292,38 @@ end
 -- Convert pandoc alignment to something HTML can use.
 -- align is AlignLeft, AlignRight, AlignCenter, or AlignDefault.
 function html_align(align)
-  if align == 'AlignLeft' then
-    return 'left'
-  elseif align == 'AlignRight' then
-    return 'right'
-  elseif align == 'AlignCenter' then
-    return 'center'
+  if align == "AlignLeft" then
+    return "left"
+  elseif align == "AlignRight" then
+    return "right"
+  elseif align == "AlignCenter" then
+    return "center"
   else
-    return 'left'
+    return "left"
   end
 end
 
-
-
 function table.shallow_copy(t)
   local t2 = {}
-  for k,v in pairs(t) do
+  for k, v in pairs(t) do
     t2[k] = v
   end
   return t2
 end
 
-
 function CaptionedImage(src, tit, caption, attr)
-   local prefix = ""
-   if attr and attr['id'] ~= "" then
-      prefix = AnchorRef(attr['id'])
-   end
+  local prefix = ""
+  if attr and attr["id"] ~= "" then
+    prefix = AnchorRef(attr["id"])
+  end
 
-   attr_cpy = table.shallow_copy(attr)
-   attr_cpy['id'] = "captioned-image"
-   return Div('<table><tbody><tr><td><ac:image><ri:attachment ri:filename="' .. escape(src,true) .. '" /></ac:image></td></tr><tr><td>' .. escape(caption) .. '</td></tr></tbody></table>', attr_cpy)
+  attr_cpy = table.shallow_copy(attr)
+  attr_cpy["id"] = "captioned-image"
+  return Div(
+    '<table><tbody><tr><td><ac:image><ri:attachment ri:filename="' ..
+      escape(src, true) .. '" /></ac:image></td></tr><tr><td>' .. escape(caption) .. "</td></tr></tbody></table>",
+    attr_cpy
+  )
 end
 
 -- Caption is a string, aligns is an array of strings,
@@ -339,61 +335,56 @@ function Table(caption, aligns, widths, headers, rows)
     table.insert(buffer, s)
   end
   add("<table>")
-  if caption ~= "" then
-    add("<caption>" .. caption .. "</caption>")
-  end
-  if widths and widths[1] ~= 0 then
-    for _, w in pairs(widths) do
-      add('<col width="' .. string.format("%d%%", w * 100) .. '" />')
-    end
-  end
+  add("<tbody>")
   local header_row = {}
   local empty_header = true
   for i, h in pairs(headers) do
     local align = html_align(aligns[i])
-    table.insert(header_row,'<th align="' .. align .. '">' .. h .. '</th>')
+    table.insert(header_row, '<th align="' .. align .. '">' .. h .. "</th>")
     empty_header = empty_header and h == ""
   end
   if empty_header then
     head = ""
   else
-    add('<tr class="header">')
-    for _,h in pairs(header_row) do
+    add("<th>")
+    for _, h in pairs(header_row) do
       add(h)
     end
-    add('</tr>')
+    add("</th>")
   end
   local class = "even"
   for _, row in pairs(rows) do
     class = (class == "even" and "odd") or "even"
-    add('<tr class="' .. class .. '">')
-    for i,c in pairs(row) do
-      add('<td align="' .. html_align(aligns[i]) .. '">' .. c .. '</td>')
+    add("<tr>")
+    for i, c in pairs(row) do
+      add('<td align="' .. html_align(aligns[i]) .. '">' .. c .. "</td>")
     end
-    add('</tr>')
+    add("</tr>")
   end
-  add('</table')
-  return table.concat(buffer,'\n')
+  add("</tbody>")
+  add("</table")
+  return table.concat(buffer, "\n")
 end
 
 function RawBlock(format, str)
   if format == "html" then
     return str
   else
-    return ''
+    return ""
   end
 end
 
 function Div(s, attr)
-  local div_text = [[
+  local div_text =
+    [[
 <ac:structured-macro ac:macro-id="57855606-2855-47df-ac05-f2cb358f1e23" ac:name="div" ac:schema-version="1">]]
-  for x,y in pairs(attr) do
+  for x, y in pairs(attr) do
     if y and y ~= "" then
-       div_text = div_text .. string.format('  <ac:parameter ac:name="%s">%s</ac:parameter>', x, y)
+      div_text = div_text .. string.format('  <ac:parameter ac:name="%s">%s</ac:parameter>', x, y)
     end
   end
 
-  div_text = div_text .. string.format('  <ac:rich-text-body>%s</ac:rich-text-body>\n</ac:structured-macro>', s)
+  div_text = div_text .. string.format("  <ac:rich-text-body>%s</ac:rich-text-body>\n</ac:structured-macro>", s)
   return div_text
 end
 
@@ -405,10 +396,10 @@ end
 -- all of the functions you need for the custom writer, so it's useful
 -- to include when you're working on a writer.
 local meta = {}
-meta.__index =
-  function(_, key)
-    io.stderr:write(string.format("WARNING: Undefined function '%s'\n",key))
-    return function() return "" end
+meta.__index = function(_, key)
+  io.stderr:write(string.format("WARNING: Undefined function '%s'\n", key))
+  return function()
+    return ""
   end
+end
 setmetatable(_G, meta)
-
